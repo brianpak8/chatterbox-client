b786f4629b471312d70dec54941db69e43876330
Hack Reactor Student <student@hackreactor.com>
2017-10-21 16:53:31 -0700

    TOOK THE WHOLE DAY FOR A BUTTON

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,35 +1,30 @@
 // YOUR CODE HERE:
-/*
-libraries
-keys
-
-*/
-
-
 
 var app = {
   server: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages'
 };
-$('document').ready(function() {
+$('document').ready(function(event) {
   app.init();
-  console.log('hi');
 
-  //$('.username').on('click', app.handleUsernameClick);
-  // for(var i = 0; i < text.length; i++) {
-  //   console.log(this.renderMessage(text[i].data));
-  // }
+  $('#inputcontainer').on('submit', function(event) {
+    console.log('running !!!!!!!!!!!!!!!!!!');
+    event.preventDefault();
+    app.handleSubmit(event);
+  });
+
 });
-// app.fetch();
 
 
 app.init = function() {
-  $('.name').on('click', function() {
-    app.handleUsernameClick();
-    alert(1);
-  });
-  $('#submit').on('submit', function() {
-    app.handleSubmit();
-  });
+  // $('.name').on('click', function() {
+  //   app.handleUsernameClick();
+  //   alert(1);
+  // });
+  // $('#submit').on('submit', function(message) {
+  //   app.handleSubmit(message);
+  // });
+
+  //preventdefault
   app.fetch();
 };
 
@@ -37,7 +32,7 @@ app.send = function(message) {
   $.ajax({
     url: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages',
     type: 'POST',
-    data: message,
+    data: JSON.stringify(message),
     contentType: 'application/json',
     success: function (data) {
       console.log('chatterbox: Message sent');
@@ -65,36 +60,15 @@ app.fetch = function() {
 };
 
 app.clearMessages = function() {
-  // console.log(document.body.getElementById('chats'));
-  // var target = $('#chats').children();
-  // for (var i = 0; i < target.length; i++) {
-  //   target.pop();
-  // }
   var childrenMessages = $('#chats').children();
   for (var i = 0; i < childrenMessages.length; i++) {
     childrenMessages[i].remove();
   }
 };
-//message is an object
 app.renderMessage = function(message) {
-  // var $message = <
-  // message = message.text();
-  //console.log(message);
-  //`<div class = user> ${message.username} ${message.text} ${message.createAt} </div>`
-  // console.log(message.createAt);
   var temp = $('.message').text(`${message.username} ${message.text} ${message.createdAt}`);
   var filtered = filterXSS(`<div class = user> <span class="name"> ${message.username} </span>: ${message.text} ${message.createdAt}</div>`);
-  // console.log(temp);
-  // // //console.log(temp)
-  // // $('#chats').append(temp);
-  // var re = new Regex("[;\\/:*?\"<>|&']");
-  // var output = re.replace(inputString, " ");
-  // var msg = output(message.username);
-  // var createAt = output(message.createAt);
-  // var text = output(message.text);
   $('#chats').append(filtered);
-  //$('#chats').append(`<div class = user> <span class="name"> ${msg} </span>: ${createAt} ${text} </div>`);
-  // $('#chats').append(`<div class = user> <span class="name">${message.username}</span>: ${message.text} ${message.createdAt} </div>`);
 };
 
 app.renderRoom = function(name) {
@@ -105,22 +79,29 @@ app.handleUsernameClick = function() {
   alert(this.username);
 };
 
-app.handleSubmit = function(message) {
+app.handleSubmit = function(event) {
+  event.preventDefault();
+  var msg = $('.input').val();
+  console.log(msg, '!!!!!!!!!!!!!!!!!!!!!!!!!');
+
+  var message = {
+    username: window.location.href.split('=')[1],
+    text: msg
+  }
   app.send(message);
-
 };
 
-var getElementsByClassName = function(className, node) {
-  node = node || document.body;
-  var result = [];
-  var nodeNames = node.className.split(' ');
-  if ( nodeNames.includes(className) ) {
-    result.push(node);
-  }
-  var childrenOfNode = node.children;
-  for (var i = 0; i < childrenOfNode.length; i++)  {
-    var tempChild = getElementsByClassName(className, childrenOfNode[i]);
-    result = result.concat(tempChild);
-  }
-  return result;
-};
+// var getElementsByClassName = function(className, node) {
+//   node = node || document.body;
+//   var result = [];
+//   var nodeNames = node.className.split(' ');
+//   if ( nodeNames.includes(className) ) {
+//     result.push(node);
+//   }
+//   var childrenOfNode = node.children;
+//   for (var i = 0; i < childrenOfNode.length; i++)  {
+//     var tempChild = getElementsByClassName(className, childrenOfNode[i]);
+//     result = result.concat(tempChild);
+//   }
+//   return result;
+// };
